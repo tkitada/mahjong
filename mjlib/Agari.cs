fdd6f66af73375506d6e86edc82937174f94b252
kitada <kitada.taihei@Horizon.co.jp>
2020-06-01 07:43:28 +0900

    とりあえず2

---
+++ mjlib/Agari.cs
@@ -0,0 +1,32 @@
+﻿using mjlib.Tiles;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mjlib
+{
+    internal class Agari
+    {
+        public static bool IsAgari(TilesSet tilesSet, List<Tiles34> openSets)
+        {
+            var tiles = new TilesSet(tilesSet.Select(t => t).ToList());
+
+            if (openSets.Count != 0)
+            {
+                var isolatedTiles = tiles.FindIsolatedTileIndices();
+                foreach (var meld in openSets)
+                {
+                    if (isolatedTiles.Count == 0) break;
+
+                    var isolatedTile = isolatedTiles[0];
+                    isolatedTiles.RemoveAt(0);
+
+                    tiles[meld[0].Value] -= 1;
+                    tiles[meld[1].Value] -= 1;
+                    tiles[meld[2].Value] -= 1;
+                    tiles[isolatedTile.Value] = 3;
+                }
+            }
+
+        }
+    }
+}
